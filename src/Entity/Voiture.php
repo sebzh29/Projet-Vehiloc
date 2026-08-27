350a02c1bafedf9923834847353aac995556d1a5
Sébastien Glippa <sebastien.glippa@hotmail.fr>
2026-08-27 09:27:32 +0200

    Création de l'entité Voiture

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $monthlyPrice = null;
+
+    #[ORM\Column]
+    private ?int $dailyPrice = null;
+
+    #[ORM\Column]
+    private ?int $places = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $motor = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getMonthlyPrice(): ?int
+    {
+        return $this->monthlyPrice;
+    }
+
+    public function setMonthlyPrice(int $monthlyPrice): static
+    {
+        $this->monthlyPrice = $monthlyPrice;
+
+        return $this;
+    }
+
+    public function getDailyPrice(): ?int
+    {
+        return $this->dailyPrice;
+    }
+
+    public function setDailyPrice(int $dailyPrice): static
+    {
+        $this->dailyPrice = $dailyPrice;
+
+        return $this;
+    }
+
+    public function getPlaces(): ?int
+    {
+        return $this->places;
+    }
+
+    public function setPlaces(int $places): static
+    {
+        $this->places = $places;
+
+        return $this;
+    }
+
+    public function getMotor(): ?string
+    {
+        return $this->motor;
+    }
+
+    public function setMotor(string $motor): static
+    {
+        $this->motor = $motor;
+
+        return $this;
+    }
+}
